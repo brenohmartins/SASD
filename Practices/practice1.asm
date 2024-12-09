df3b8dd799aa7f4a1047c2a1f613d82882826283
breno-martins <breno.henrique.silva@ee.ufcg.edu.br>
2024-12-09 11:22:48 -0300

    add practices to repository

---
+++ Practices/practice1.asm
@@ -0,0 +1,18 @@
+#  -----------instruction of sum-----------
+# in C a simple sum code: a = b + c;
+# in Assembly 
+
+add a, b, c
+
+# 'add' is the operator 
+# 'b,c' is the valors to use in operation
+# 'a' is the destination of the result of the operation
+
+# -----------instruction of sub-----------
+# inc C a simple code of a subtration is d = e - f;
+# in Assembly we have
+
+sub d, e, f
+
+# 'sub' is the operator, and follow the same logic of the 'sum' parameters
+
---
+++ Practices/practice2.asm
@@ -0,0 +1,9 @@
+#-----------Multiple Instructions-----------
+
+#in C we hve the peratio -> a = b + c - d;
+
+# in Assembly RISC-V we can translate to:
+add t, b, c; # t = b + c
+sub a, t, d; # a =( t - d )= b + c - d
+
+# We created a temporary variable 't' to make it possible to perform the operation of 'a'
