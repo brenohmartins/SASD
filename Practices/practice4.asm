a09b315cfcc5e0f717ad1791e39ec725a9acacae
breno-martins <breno.henrique.silva@ee.ufcg.edu.br>
2024-12-11 12:36:27 -0300

    new comments in practice 4

--- Practices/practice4.asm
+++ Practices/practice4.asm
@@ -21,3 +21,15 @@ lw destiny, deslocament(base)
 
 # In Assembly RISC-V
 lw s3, 1(zero) # read word from memory 1 to s3
+
+
+# store word -> sw
+
+#format: 
+#for example, if t4 holds the value 0xFEEDCABB, then after this instruction completes, 
+# word 3 in memory will contain that value
+sw t4, 0x3(zero) # write the value in t4 in word 3
+#addres is: (0 + 0x3) = 3
+
+#----RISC-V is byte-addressable, not word-addressable--------
+#the address of memory word 2 is 2 × 4 = 8
