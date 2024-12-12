db508935e58ec4e649e9557155566b523f8737d2
breno_martins <breno.henrique.silva@ee.ufcg.edu.br>
2024-12-12 11:07:47 -0300

    update practice9

--- Practices/practice9.asm
+++ Practices/practice9.asm
@@ -27,3 +27,30 @@ addi, s1, s1, 1   # x = x +1
 j while           # jump to the while label
 
 done:           # end of program
+
+# when we use the 'for' loop, remember that it works as follows => for (initialization; condition; loop operation)
+
+#For exemple, code in C:
+
+# int sum = 0;
+# int i;
+
+#for (i=0; i!=10; i = i+1){
+#   sum = sum + i;
+#}
+
+#In assembly we have
+
+#s0 = i, s1 = sum
+
+addi s1, zero, 0  # sum = 0
+addi s0, zero, zero  # i = 0
+addi t0, zero, 10 # t = 10
+
+for: #label
+beq s0, t0, done # for i = 10 jump to done
+add s1, s1, s0 # sum = sum + i
+addi s0, s0, 1 # i = i + 1 
+j for # jump to label for
+
+done:
