40fb8b4ad617428b61d21306806fbb74182dd3a1
breno_martins <breno.henrique.silva@ee.ufcg.edu.br>
2024-12-12 14:31:25 -0300

    update exercise1

--- Practices/exercise1.asm
+++ Practices/exercise1.asm
@@ -1,5 +1,6 @@
 # Convert the code below to RISC-V Assembly
 
+#1
 #C Code ->
 
 # int a = 5;
@@ -24,3 +25,28 @@ add t3, s0, t2    # t3 = a + t2
 
 slli s3, t3, 3    # s3 = 4 * t3
 #remember 2^3 = 8, 2^2=4, 2^1=2, to make the shifts
+
+#2.
+
+# C code ->
+
+# int i;
+# int a = 0;
+# for(i=15; i<30; i++)
+# a = a + i;
+
+# assembly code: 
+
+# s0 = a, s1 = i
+
+addi s0, zero, 0 # a = 0
+addi s1, zero, 15 # i = 15
+addi t0, zero, 30 # to = 30
+
+for: 
+bge s1, t0, done
+add s0, s0 , s1
+addi s1, s1, 1
+j for
+
+done:
