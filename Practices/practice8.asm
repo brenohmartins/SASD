d112151c0a4fd5d2ac34454d03656a66ce65ac1a
breno_martins <breno.henrique.silva@ee.ufcg.edu.br>
2024-12-12 10:34:17 -0300

    add practice8

---
+++ Practices/practice8.asm
@@ -0,0 +1,32 @@
+#------------CONDITIONAL SENTENCES AND LOOPS-------------
+
+# Conditional sentences: if, if/else
+# Loops: while, for
+
+# Code in C:
+# if (i==j)
+#   f=g+h;
+# f = f - i;
+
+# In Assembly RISC-V
+
+# s0=f, s1=g, s2=h, s4=i, s4=j
+# in assembly we test the opposing case (i!=j) using 'bne'
+
+bne s3, s4, L1  # if (i!=j)
+add s0, s1, s2 #f = g + h
+
+L1: #label
+sub s0, s0, s3 #f = f - i
+
+# for example the same code but with an else we have in assembly:
+
+# s0 = f, s1 = g, s2 = h
+# s3 = i, s4 = j
+
+bne s3, s4, else # if (i!=j)
+add s0, s1, s2 #f = g + h
+j done # end of program
+
+else: #label
+sub s0, s0, s3 # f = f - i
